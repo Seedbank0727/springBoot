4b75b78bd9eba30db78f78d8b4bc38cfc327435d
Juwon <jjw1578@gmail.com>
2023-08-29 16:30:49 +0900

    JPAcontroller and CRUD

--- src/main/java/com/kosa/rest_service/user/UserJpaController.java
+++ src/main/java/com/kosa/rest_service/user/UserJpaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -28,40 +29,37 @@ public class UserJpaController {
         return userRepository.findAll();
     }
 
-//    @GetMapping("/users/{id}")
-//    public EntityModel<User> retrieveUser(@PathVariable int id){
-//        User user = service.findOne(id);
-//
-//        if(user == null) {
-//            throw new UserNotFoundException(String.format("ID[%s] not found", id));
-//        }
-//
-//        return EntityModel.of(user,
-//                linkTo(methodOn(UserJpaController.class).retrieveAllUsers()).withRel("all-users"));
-//    }
-//
-//    @PostMapping("/users")
-//    public User createUser(@Valid @RequestBody User user) {
-//        User savedUser = service.save(user);
-//
-//        // 클라이언트가 방금 입력한 정보를 확인하고 싶을 수 있으므로
-//        // Header에 URI 정보를 준다.
-//        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-//                .path("/{id}")
-//                .buildAndExpand(savedUser.getId())
-//                .toUri();
-//
-//        // 이렇게 리턴하면 201 OK가 된다.
-//        return user;
-//    }
-//
-//    @DeleteMapping("/users/{id}")
-//    public void deleteUser(@PathVariable int id) {
-//        User user = service.deleteById(id);
-//
-//        if(user == null) {
-//            throw new UserNotFoundException(String.format("ID[%s] not found", id));
-//        }
-//    }
+    @GetMapping("/users/{id}")
+    public EntityModel<Optional<User>> retrieveUser(@PathVariable int id){
+        Optional<User> user = userRepository.findById(id); // 데이터가 없을 수 있기 때문에 optional 객체를 선언
+
+        if(!user.isPresent()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return EntityModel.of(user,
+                linkTo(methodOn(UserJpaController.class).retrieveAllUsers()).withRel("all-users"));
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+
+
+        // 클라이언트가 방금 입력한 정보를 확인하고 싶을 수 있으므로
+        // Header에 URI 정보를 준다.
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        // 이렇게 리턴하면 201 OK가 된다.
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user2 values (1,now(),'user1','test1','701010-11111111');
-insert into user2 values (2,now(),'user2','test2','801010-11111111');
-insert into user2 values (3,now(),'user3','test3','901010-11111111');
+insert into user2 values (9001,now(),'user1','test1','701010-11111111');
+insert into user2 values (9002,now(),'user2','test2','801010-11111111');
+insert into user2 values (9003,now(),'user3','test3','901010-11111111');

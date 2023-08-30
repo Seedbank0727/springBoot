b8b55814f8c3ef0671d5880115563885835f12eb
Juwon <jjw1578@gmail.com>
2023-08-30 11:49:12 +0900

    shop 폴더 추가 및 테스트 파일

---
+++ src/test/java/com/kosa/rest_service/shop/MemberRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.kosa.rest_service.shop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    @Test
+    void findByEmail() {
+    }
+}
